346d8bcedc7c91b90f3e340d8e1a70cfe0972cea
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:54:57 +0530

    account_operations_spec.sql

--- week2_exercises/07.packages/scenario3/account_operations_spec.sql
+++ week2_exercises/07.packages/scenario3/account_operations_spec.sql
@@ -1,1 +1,17 @@
-a
+CREATE OR REPLACE PACKAGE AccountOperations AS
+    PROCEDURE OpenAccount(
+        p_account_id IN NUMBER,
+        p_customer_id IN NUMBER,
+        p_account_type IN VARCHAR2,
+        p_balance IN NUMBER
+    );
+
+    PROCEDURE CloseAccount(
+        p_account_id IN NUMBER
+    );
+
+    FUNCTION GetTotalBalance(
+        p_customer_id IN NUMBER
+    ) RETURN NUMBER;
+END AccountOperations;
+/
