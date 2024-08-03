84e1e751a70408ffb6e3e352af0f8165179839e9
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:51:33 +0530

    customer_management_body.sql

--- week2_exercises/07.packages/scenario1/customer_management_body.sql
+++ week2_exercises/07.packages/scenario1/customer_management_body.sql
@@ -1,1 +1,20 @@
-A
+CREATE OR REPLACE PACKAGE CustomerManagement AS
+    PROCEDURE AddCustomer(
+        p_customer_id IN NUMBER,
+        p_name IN VARCHAR2,
+        p_dob IN DATE,
+        p_balance IN NUMBER
+    );
+
+    PROCEDURE UpdateCustomer(
+        p_customer_id IN NUMBER,
+        p_name IN VARCHAR2,
+        p_dob IN DATE,
+        p_balance IN NUMBER
+    );
+
+    FUNCTION GetCustomerBalance(
+        p_customer_id IN NUMBER
+    ) RETURN NUMBER;
+END CustomerManagement;
+/
