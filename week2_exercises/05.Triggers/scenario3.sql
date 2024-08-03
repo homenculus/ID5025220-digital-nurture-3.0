6e926be01dccf83168d963ef9b7d9997dfd3e73e
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:33:14 +0530

    scenario3.sql

--- week2_exercises/05.Triggers/scenario3.sql
+++ week2_exercises/05.Triggers/scenario3.sql
@@ -1,1 +1,21 @@
-a
+CREATE OR REPLACE TRIGGER CheckTransactionRules
+BEFORE INSERT ON Transactions
+FOR EACH ROW
+DECLARE
+    v_balance NUMBER;
+BEGIN
+    IF :NEW.TransactionType = 'Withdrawal' THEN
+        SELECT balance INTO v_balance
+        FROM accounts
+        WHERE account_id = :NEW.AccountID;
+
+        IF :NEW.Amount > v_balance THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Withdrawal amount exceeds balance.');
+        END IF;
+    ELSIF :NEW.TransactionType = 'Deposit' THEN
+        IF :NEW.Amount <= 0 THEN
+            RAISE_APPLICATION_ERROR(-20002, 'Deposit amount must be positive.');
+        END IF;
+    END IF;
+END CheckTransactionRules;
+/
