35833c7946709ced6d3d9eb04d06d7d443277ae1
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 05:22:45 +0530

    Bookservice.java

--- week2_exercises/08.configuring a basic spring application/src/main/java/com/library/Bookservice.java
+++ week2_exercises/08.configuring a basic spring application/src/main/java/com/library/Bookservice.java
@@ -1,1 +1,15 @@
-a
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String book) {
+        bookRepository.addBook(book);
+    }
+}
