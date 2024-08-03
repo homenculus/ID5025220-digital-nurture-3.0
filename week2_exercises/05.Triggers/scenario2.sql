29bf731072f9c28fe5d51c498593d987b9523753
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:32:53 +0530

    scenario2.sql

--- week2_exercises/05.Triggers/scenario2.sql
+++ week2_exercises/05.Triggers/scenario2.sql
@@ -1,1 +1,8 @@
-a
+CREATE OR REPLACE TRIGGER LogTransaction
+AFTER INSERT ON Transactions
+FOR EACH ROW
+BEGIN
+    INSERT INTO AuditLog (TransactionID, TransactionDate, OldAmount, NewAmount, ChangedBy)
+    VALUES (:NEW.TransactionID, SYSDATE, :NEW.Amount, :NEW.Amount, USER);
+END LogTransaction;
+/
