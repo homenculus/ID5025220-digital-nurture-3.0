3db9b8e79b54406a7bac0aaa1d611d995d7b2c2f
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:52:32 +0530

    customer_management_body.sql

--- week2_exercises/07.packages/scenario1/customer_management_body.sql
+++ week2_exercises/07.packages/scenario1/customer_management_body.sql
@@ -1,20 +1,46 @@
-CREATE OR REPLACE PACKAGE CustomerManagement AS
+CREATE OR REPLACE PACKAGE BODY CustomerManagement AS
+
     PROCEDURE AddCustomer(
         p_customer_id IN NUMBER,
         p_name IN VARCHAR2,
         p_dob IN DATE,
         p_balance IN NUMBER
-    );
+    ) IS
+    BEGIN
+        INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+        VALUES (p_customer_id, p_name, p_dob, p_balance, SYSDATE);
+        COMMIT;
+    END AddCustomer;
 
     PROCEDURE UpdateCustomer(
         p_customer_id IN NUMBER,
         p_name IN VARCHAR2,
         p_dob IN DATE,
         p_balance IN NUMBER
-    );
+    ) IS
+    BEGIN
+        UPDATE Customers
+        SET Name = p_name,
+            DOB = p_dob,
+            Balance = p_balance,
+            LastModified = SYSDATE
+        WHERE CustomerID = p_customer_id;
+        COMMIT;
+    END UpdateCustomer;
 
     FUNCTION GetCustomerBalance(
         p_customer_id IN NUMBER
-    ) RETURN NUMBER;
+    ) RETURN NUMBER IS
+        v_balance NUMBER;
+    BEGIN
+        SELECT Balance INTO v_balance
+        FROM Customers
+        WHERE CustomerID = p_customer_id;
+        RETURN v_balance;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN NULL;
+    END GetCustomerBalance;
+
 END CustomerManagement;
 /
