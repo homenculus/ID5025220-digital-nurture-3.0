1088b234a10295c097abdc1199232442164b5f02
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:24:38 +0530

    scenario2.sql

--- week2_exercises/01.control structure/scenario2.sql
+++ week2_exercises/01.control structure/scenario2.sql
@@ -1,1 +1,13 @@
-a
+BEGIN
+    FOR customer_record IN (SELECT customer_id, balance
+                            FROM customers) LOOP
+        IF customer_record.balance > 10000 THEN
+            UPDATE customers
+            SET is_vip = TRUE
+            WHERE customer_id = customer_record.customer_id;
+        END IF;
+    END LOOP;
+    
+    COMMIT;
+END;
+/
