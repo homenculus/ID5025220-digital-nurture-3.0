b1368ef7b9980b01c4fe9ad9ab632f32f62b3887
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:28:07 +0530

    scenario3.sql

--- week2_exercises/03.stored procedures/scenario3.sql
+++ week2_exercises/03.stored procedures/scenario3.sql
@@ -1,1 +1,32 @@
-a
+CREATE OR REPLACE PROCEDURE TransferFunds (
+    p_from_account_id IN NUMBER,
+    p_to_account_id IN NUMBER,
+    p_amount IN NUMBER
+) AS
+    v_balance NUMBER;
+BEGIN
+    SELECT balance INTO v_balance
+    FROM accounts
+    WHERE account_id = p_from_account_id;
+    
+    IF v_balance < p_amount THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Insufficient funds.');
+    END IF;
+
+    UPDATE accounts
+    SET balance = balance - p_amount
+    WHERE account_id = p_from_account_id;
+
+    UPDATE accounts
+    SET balance = balance + p_amount
+    WHERE account_id = p_to_account_id;
+    
+    COMMIT;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Account not found.');
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END TransferFunds;
+/
