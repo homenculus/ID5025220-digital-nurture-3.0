f365e4444ad953d93216f80a26e886292987760a
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:27:40 +0530

    scenario2.sql

--- week2_exercises/03.stored procedures/scenario2.sql
+++ week2_exercises/03.stored procedures/scenario2.sql
@@ -1,1 +1,12 @@
-a
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus (
+    p_department_id IN NUMBER,
+    p_bonus_percentage IN NUMBER
+) AS
+BEGIN
+    UPDATE employees
+    SET salary = salary * (1 + p_bonus_percentage / 100)
+    WHERE department_id = p_department_id;
+    
+    COMMIT;
+END UpdateEmployeeBonus;
+/
