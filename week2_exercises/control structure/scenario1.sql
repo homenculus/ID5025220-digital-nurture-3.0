3ca093e5b27962e6881e8797548dd6e2a9e5cbff
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:13:02 +0530

    scenario1.sql

--- week2_exercises/control structure/scenario1.sql
+++ week2_exercises/control structure/scenario1.sql
@@ -1,1 +1,14 @@
-a
+BEGIN
+    FOR customer_record IN (SELECT customer_id, age, loan_interest_rate
+                            FROM customers
+                            JOIN loans ON customers.customer_id = loans.customer_id) LOOP
+        IF customer_record.age > 60 THEN
+            UPDATE loans
+            SET loan_interest_rate = loan_interest_rate - 0.01
+            WHERE customer_id = customer_record.customer_id;
+        END IF;
+    END LOOP;
+    
+    COMMIT;
+END;
+/
