4c2353bdd9a253efc61f0e441f0502abf33b6e95
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 04:29:02 +0530

    scenario1.sql

--- week2_exercises/04.Functions/scenario1.sql
+++ week2_exercises/04.Functions/scenario1.sql
@@ -1,1 +1,12 @@
-a
+CREATE OR REPLACE FUNCTION CalculateAge (
+    p_date_of_birth IN DATE
+) RETURN NUMBER AS
+    v_age NUMBER;
+BEGIN
+    SELECT FLOOR(MONTHS_BETWEEN(SYSDATE, p_date_of_birth) / 12)
+    INTO v_age
+    FROM dual;
+
+    RETURN v_age;
+END CalculateAge;
+/
