65b76957c4a8d0ab23c630f061060115106bb113
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 13:05:44 +0530

    LibraryAppRunner.java

--- week2_exercises/11.creating and configuring a maven project/LibraryManagement/src/main/java/com/Library/LibraryAppRunner.java
+++ week2_exercises/11.creating and configuring a maven project/LibraryManagement/src/main/java/com/Library/LibraryAppRunner.java
@@ -1,1 +1,13 @@
-a
+package com.library;
+
+import com.library.service.LibraryService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryAppRunner {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        LibraryService libraryService = (LibraryService) context.getBean("libraryService");
+        libraryService.addBook("Effective Java");
+    }
+}
