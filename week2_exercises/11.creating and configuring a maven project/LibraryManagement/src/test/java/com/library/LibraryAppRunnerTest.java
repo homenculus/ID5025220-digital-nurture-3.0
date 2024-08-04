4a974b42c2c3d71d4304cff0bc5462bf77a6aba9
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 13:07:49 +0530

    LibraryAppRunnerTest.java

--- week2_exercises/11.creating and configuring a maven project/LibraryManagement/src/test/java/com/library/LibraryAppRunnerTest.java
+++ week2_exercises/11.creating and configuring a maven project/LibraryManagement/src/test/java/com/library/LibraryAppRunnerTest.java
@@ -1,1 +1,16 @@
-a
+package com.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.LibraryService;
+
+public class LibraryAppRunnerTest {
+
+    @Test
+    public void testApplicationContext() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        LibraryService libraryService = context.getBean(LibraryService.class);
+        libraryService.addBook("JUnit Testing in Spring");
+    }
+}
