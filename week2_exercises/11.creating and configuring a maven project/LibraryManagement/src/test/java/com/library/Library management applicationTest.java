5e98356040e9c8f6f48bfca65b7435ba2c908517
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-04 12:46:52 +0530

    Library management applicationTest.java

--- week2_exercises/11.creating and configuring a maven project/LibraryManagement/src/test/java/com/library/Library management applicationTest.java
+++ week2_exercises/11.creating and configuring a maven project/LibraryManagement/src/test/java/com/library/Library management applicationTest.java
@@ -1,1 +1,16 @@
-a
+package com.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+public class LibraryManagementApplicationTest {
+
+    @Test
+    public void testApplicationContext() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = context.getBean(BookService.class);
+        bookService.addBook("JUnit in Action");
+    }
+}
