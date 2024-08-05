b4bbbc6d56843966b8bbade778f3fcd04bb6b8df
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 15:28:45 +0530

    scenario3.sql

--- week2_exercises/06.cursors/scenario3.sql
+++ week2_exercises/06.cursors/scenario3.sql
@@ -2,25 +2,23 @@ DECLARE
     CURSOR loan_cursor IS
         SELECT loan_id, interest_rate
         FROM loans;
-    
-    v_loan_id NUMBER;
-    v_old_interest_rate NUMBER;
-    v_new_interest_rate CONSTANT NUMBER := 0.05; -- Example new interest rate
-    
+
+    v_loan_id loans.loan_id%TYPE;
+    v_current_rate loans.interest_rate%TYPE;
+    v_new_rate CONSTANT NUMBER := 0.05; -- Example new interest rate
+
 BEGIN
-    FOR loan_rec IN loan_cursor LOOP
-        v_loan_id := loan_rec.loan_id;
-        v_old_interest_rate := loan_rec.interest_rate;
-        
+    OPEN loan_cursor;
+    LOOP
+        FETCH loan_cursor INTO v_loan_id, v_current_rate;
+        EXIT WHEN loan_cursor%NOTFOUND;
+
         UPDATE loans
-        SET interest_rate = v_new_interest_rate
+        SET interest_rate = v_new_rate
         WHERE loan_id = v_loan_id;
-        
-        DBMS_OUTPUT.PUT_LINE('Updated Loan ID: ' || v_loan_id || 
-                             ', Old Interest Rate: ' || v_old_interest_rate ||
-                             ', New Interest Rate: ' || v_new_interest_rate);
     END LOOP;
-    
+    CLOSE loan_cursor;
+
     COMMIT;
 END;
 /
