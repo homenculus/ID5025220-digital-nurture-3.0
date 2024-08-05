1be4303a7439c1820c726620efe9920871c108b9
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 09:59:10 +0530

    BookService.java

--- week2_exercises/14.Implementing constructor and Setter Injection/LibraryManagement/src/main/java/com/BookService.java
+++ week2_exercises/14.Implementing constructor and Setter Injection/LibraryManagement/src/main/java/com/BookService.java
@@ -1,1 +1,20 @@
-a
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    
+    private BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String book) {
+        bookRepository.addBook(book);
+    }
+}
