d0c314dcf6711e29e6031c23e5eb82a444d482aa
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 16:29:16 +0530

    BookRepository.java

--- week2_exercises/16.Creating a spring Boot Application/LibraryManagement/src/main/java/com/repository/BookRepository.java
+++ week2_exercises/16.Creating a spring Boot Application/LibraryManagement/src/main/java/com/repository/BookRepository.java
@@ -1,1 +1,9 @@
-a
+package com.library.repository;
+
+import com.library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
