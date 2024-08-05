675bea907a5a0e7d0886f5a27e2ee5c0becbc3ca
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 16:26:46 +0530

    LibraryManagementApplicationTests.java

--- week2_exercises/16.Creating a spring Boot Application/LibraryManagement/src/test/java/com/library/LibraryManagementApplicationTests.java
+++ week2_exercises/16.Creating a spring Boot Application/LibraryManagement/src/test/java/com/library/LibraryManagementApplicationTests.java
@@ -1,1 +1,24 @@
-a
+package com.library;
+
+import com.library.entity.Book;
+import com.library.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class LibraryManagementApplicationTests {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    public void contextLoads() {
+        Book book = new Book("Test Book", "Test Author");
+        Book savedBook = bookRepository.save(book);
+        assertThat(savedBook).isNotNull();
+        assertThat(savedBook.getId()).isGreaterThan(0);
+    }
+}
