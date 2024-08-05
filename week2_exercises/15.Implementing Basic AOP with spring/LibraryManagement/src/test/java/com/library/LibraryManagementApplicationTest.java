52e818c3e5a51bc0579a0a7ec6a46905b651c892
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 10:23:56 +0530

    Update LibraryManagementApplicationTest.java

--- week2_exercises/15.Implementing Basic AOP with spring/LibraryManagement/src/test/java/com/library/LibraryManagementApplicationTest.java
+++ week2_exercises/15.Implementing Basic AOP with spring/LibraryManagement/src/test/java/com/library/LibraryManagementApplicationTest.java
@@ -1,1 +1,16 @@
-a
+package com.library;
+
+import com.library.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplicationTest {
+
+    @Test
+    public void testAopLogging() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = context.getBean(BookService.class);
+        bookService.addBook("AOP in Spring");
+    }
+}
