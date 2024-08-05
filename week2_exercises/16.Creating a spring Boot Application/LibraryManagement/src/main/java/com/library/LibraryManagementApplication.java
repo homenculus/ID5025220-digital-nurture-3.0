b3305e13a90fdda04bb68798042bbddef287c77e
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 16:28:45 +0530

    LibraryManagementApplication.java

--- week2_exercises/16.Creating a spring Boot Application/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
+++ week2_exercises/16.Creating a spring Boot Application/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
@@ -1,1 +1,12 @@
-a
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementApplication.class, args);
+    }
+}
