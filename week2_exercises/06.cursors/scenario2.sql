4a3ba8865f328940f240349079b5f3af7c03bba8
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 15:28:25 +0530

    scenario2.sql

--- week2_exercises/06.cursors/scenario2.sql
+++ week2_exercises/06.cursors/scenario2.sql
@@ -2,24 +2,22 @@ DECLARE
     CURSOR account_cursor IS
         SELECT account_id, balance
         FROM accounts;
-    
-    v_account_id NUMBER;
-    v_balance NUMBER;
-    v_annual_fee CONSTANT NUMBER := 50; -- Example annual fee amount
-    
+
+    v_account_id accounts.account_id%TYPE;
+    v_balance accounts.balance%TYPE;
+    v_fee CONSTANT NUMBER := 50; -- Example annual maintenance fee
 BEGIN
-    FOR account_rec IN account_cursor LOOP
-        v_account_id := account_rec.account_id;
-        v_balance := account_rec.balance;
-        
+    OPEN account_cursor;
+    LOOP
+        FETCH account_cursor INTO v_account_id, v_balance;
+        EXIT WHEN account_cursor%NOTFOUND;
+
         UPDATE accounts
-        SET balance = balance - v_annual_fee
+        SET balance = balance - v_fee
         WHERE account_id = v_account_id;
-        
-        DBMS_OUTPUT.PUT_LINE('Applied annual fee to Account ID: ' || v_account_id ||
-                             ', New Balance: ' || v_balance - v_annual_fee);
     END LOOP;
-    
+    CLOSE account_cursor;
+
     COMMIT;
 END;
 /
