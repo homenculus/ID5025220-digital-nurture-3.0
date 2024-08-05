dd630bdc5b65e0c1769e38f7d5fc69b01e56762b
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 16:27:23 +0530

    BookController.java

--- week2_exercises/16.Creating a spring Boot Application/LibraryManagement/src/main/java/com/controller/BookController.java
+++ week2_exercises/16.Creating a spring Boot Application/LibraryManagement/src/main/java/com/controller/BookController.java
@@ -1,1 +1,45 @@
-a
+package com.library.controller;
+
+import com.library.entity.Book;
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Book> getBookById(@PathVariable Long id) {
+        return bookRepository.findById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book bookDetails) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+        return bookRepository.save(book);
+    }
+
+    @DeleteMapping("/{id})
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+}
