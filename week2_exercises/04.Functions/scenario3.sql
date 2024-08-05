0f96246c881adc2f9f29c1ce5281b8954a529551
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 15:25:48 +0530

    scenario3.sql

--- week2_exercises/04.Functions/scenario3.sql
+++ week2_exercises/04.Functions/scenario3.sql
@@ -1,19 +1,11 @@
 CREATE OR REPLACE FUNCTION HasSufficientBalance (
-    p_account_id IN NUMBER,
-    p_amount IN NUMBER
-) RETURN BOOLEAN AS
+    p_account_id NUMBER,
+    p_amount NUMBER
+) RETURN BOOLEAN IS
     v_balance NUMBER;
 BEGIN
-    SELECT balance
-    INTO v_balance
-    FROM accounts
-    WHERE account_id = p_account_id;
-
-    IF v_balance >= p_amount THEN
-        RETURN TRUE;
-    ELSE
-        RETURN FALSE;
-    END IF;
+    SELECT balance INTO v_balance FROM accounts WHERE account_id = p_account_id;
+    RETURN v_balance >= p_amount;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         RETURN FALSE;
