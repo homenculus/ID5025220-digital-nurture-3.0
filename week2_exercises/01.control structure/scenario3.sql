ee006b26f2fddd4d780a92403b9447cdf4f65004
mohamed asar <128040892+homenculus@users.noreply.github.com>
2024-08-05 15:18:36 +0530

    scenario3.sql

--- week2_exercises/01.control structure/scenario3.sql
+++ week2_exercises/01.control structure/scenario3.sql
@@ -1,11 +1,23 @@
+DECLARE
+    CURSOR loan_cursor IS
+        SELECT l.loan_id, l.customer_id, c.name, l.due_date
+        FROM loans l
+        JOIN customers c ON l.customer_id = c.customer_id
+        WHERE l.due_date BETWEEN SYSDATE AND SYSDATE + 30;
+    v_loan_id loans.loan_id%TYPE;
+    v_customer_id loans.customer_id%TYPE;
+    v_customer_name customers.name%TYPE;
+    v_due_date loans.due_date%TYPE;
 BEGIN
-    FOR loan_record IN (SELECT customer_id, loan_due_date
-                        FROM loans
-                        WHERE loan_due_date BETWEEN SYSDATE AND SYSDATE + 30) LOOP
-        -- Assuming there's a procedure to send reminders
-        send_reminder(loan_record.customer_id, loan_record.loan_due_date);
+    OPEN loan_cursor;
+    LOOP
+        FETCH loan_cursor INTO v_loan_id, v_customer_id, v_customer_name, v_due_date;
+        EXIT WHEN loan_cursor%NOTFOUND;
+        
+        DBMS_OUTPUT.PUT_LINE('Reminder: Customer ' || v_customer_name || 
+                             ' (ID: ' || v_customer_id || ') has a loan (ID: ' || v_loan_id || 
+                             ') due on ' || TO_CHAR(v_due_date, 'YYYY-MM-DD'));
     END LOOP;
-    
-    COMMIT;
+    CLOSE loan_cursor;
 END;
 /
